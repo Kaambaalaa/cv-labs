5e12454225a3adc47062e995824ee43953dd2ec2
Andriy.Mishchuk <Andriy.Mishchuk@kyivstar.net>
2018-11-04 16:43:45 +0200

    remote-control

--- cv1/cv1.py
+++ cv1/cv1.py
@@ -4,28 +4,41 @@ import time
 import numpy as np
 import math
 
-image_list = []
+imageList = []
 ethalonImg = 'input\IMG_0660.JPG'
 for filename in glob.glob('input/*.jpg'):
     if (ethalonImg not in filename):
-        image_list.append(filename)
+        imageList.append(filename)
+
+imageListWithoutObj = []
+for filename in glob.glob('input_without_obj/*.jpg'):
+    if (ethalonImg not in filename):
+        imageListWithoutObj.append(filename)
+
+imageListRemote = []
+ethalonImgRemote = 'remote\detector.jpg'
+for filename in glob.glob('remote/*.jpg'):
+    if (ethalonImgRemote not in filename):
+        imageListRemote.append(filename)
 
 
-def matchCenter(img):
-    hsv_min = np.array((0, 0, 153), np.uint8)
-    hsv_max = np.array((255, 51, 255), np.uint8)
+def matchCenter(img, min, max):
+    hsv_min = np.array(min, np.uint8)
+    hsv_max = np.array(max, np.uint8)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     thresh = cv2.inRange(hsv, hsv_min, hsv_max)
     moments = cv2.moments(thresh, 1)
     dM01 = moments['m01']
     dM10 = moments['m10']
     dArea = moments['m00']
-    x = int(dM10 / dArea)
-    y = int(dM01 / dArea)
-    return x, y
+    if dArea > 100:
+        x = int(dM10 / dArea)
+        y = int(dM01 / dArea)
+        return x, y
+    return 0, 0
 
 
-def kaze_match(kps1, descs1, im2_path, f):
+def kaze_match(kps1, descs1, gray1, im2_path, f, min, max):
     start_time = time.time()
     im2 = cv2.imread(im2_path)
     gray2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
@@ -45,12 +58,17 @@ def kaze_match(kps1, descs1, im2_path, f):
                 center[0] += kps2[m.trainIdx].pt[0]
                 center[1] += kps2[m.trainIdx].pt[1]
         matchTime = time.time() - start_time
-        x, y = matchCenter(im2)
-        # похибка локалізації (відстань між реальним розміщенням предмета в кадрі та розпізнаним)
-        locError = math.sqrt((x - center[0] / len(good)) ** 2 + (y - center[1] / len(good)) ** 2)
-        info = "img: {}, mathes: {}, good: {}, delta: {}, time/size: {} sec, localization error: {}" \
+
+        info = "img: {}, mathes: {}, good: {}, delta: {}, time/size: {} sec" \
             .format(im2_path, len(matches), len(good), len(matches) - len(good),
-                    matchTime / os.path.getsize(im2_path) * 1000000, locError)
+                    matchTime / os.path.getsize(im2_path) * 1000000)
+
+        if min != None and max != None:
+            x, y = matchCenter(im2, min, max)
+            # похибка локалізації (відстань між реальним розміщенням предмета в кадрі та розпізнаним)
+            locError = math.sqrt((x - center[0] / len(good)) ** 2 + (y - center[1] / len(good)) ** 2)
+            info += ', localization error: {}'.format(locError)
+
         f.write(info + '\n')
         print(info)
         # im3 = cv2.drawMatchesKnn(gray1, kps1, gray2, kps2, good, None, flags=2)
@@ -60,16 +78,30 @@ def kaze_match(kps1, descs1, im2_path, f):
     except Exception:
         print("error")
 
-f = open("result.txt", "a")
-im1 = cv2.imread(ethalonImg)
-gray1 = cv2.cvtColor(im1, cv2.COLOR_RGB2GRAY)
-detector = cv2.AKAZE_create()
-(kps1, descs1) = detector.detectAndCompute(gray1, None)
+def processAll(list, ethalonImg, min, max):
+    im1 = cv2.imread(ethalonImg)
+    gray1 = cv2.cvtColor(im1, cv2.COLOR_RGB2GRAY)
+    detector = cv2.AKAZE_create()
+    (kps1, descs1) = detector.detectAndCompute(gray1, None)
+    for file in list:
+        kaze_match(kps1, descs1, gray1, file, f, min, max)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q') or key == 27:
+            break
 
-for file in image_list:
-    kaze_match(kps1, descs1, file, f)
-    key = cv2.waitKey(1)
-    if key & 0xFF == ord('q') or key == 27:
-        break
+
+f = open("result.txt", "a")
+min = (0, 0, 153)
+max = (255, 51, 255)
+f.write('\n' + "=======police bobik=======" + '\n')
+f.write('\n' + "=======images with obj=======" + '\n')
+processAll(imageList, ethalonImg, min, max)
+f.write("=======images without obj=======" + '\n')
+processAll(imageListWithoutObj, ethalonImg, min, max)
+min = (0, 0, 153)
+max = (255, 51, 255)
+f.write('\n' + "=======remote control=======" + '\n')
+processAll(imageListRemote, ethalonImgRemote)
+
 f.close()
 cv2.destroyAllWindows()
